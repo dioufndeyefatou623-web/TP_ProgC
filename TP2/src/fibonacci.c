2b6418599b82766c572551fd4fd257e981291e00
dioufndeyefatou623-web <dioufndeyefatou623@gmail.com>
2025-11-19 14:34:34 +0100

    Update fibonacci.c

---
+++ TP2/src/fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    unsigned long long int fib0 = 0, fib1 = 1, nextFib;
+
+    // Demande à l'utilisateur de saisir le nombre de termes
+    printf("Entrez le nombre de termes de la suite de Fibonacci: ");
+    if (scanf("%d", &n) != 1 || n < 1) {
+        printf("Entrée invalide. Veuillez entrer un entier positif.\n");
+        return 1;
+    }
+
+    printf("Suite de Fibonacci jusqu'au terme %d :\n", n);
+
+    for (i = 0; i < n; i++) {
+        if (i == 0) {
+            printf("%llu", fib0);
+        } else if (i == 1) {
+            printf(", %llu", fib1);
+        } else {
+            nextFib = fib0 + fib1;
+            printf(", %llu", nextFib);
+            fib0 = fib1;
+            fib1 = nextFib;
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
