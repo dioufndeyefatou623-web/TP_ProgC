5cbd27ca0ba63bb0f5c6662d8e3c5437fab51259
dioufndeyefatou623-web <dioufndeyefatou623@gmail.com>
2025-11-19 14:07:58 +0100

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5;  // Hauteur de la pyramide
+    int i, j;
+
+    printf("=== Generation de la pyramide ===\n\n");
+
+    // Boucle sur chaque niveau
+    for (i = 1; i <= n; i++) {
+
+        // 1) Espaces pour centrer la pyramide
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        // 2) Nombres croissants de 1 à i
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        // 3) Nombres décroissants de i-1 à 1
+        for (j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        // Passer à la ligne suivante
+        printf("\n");
+    }
+
+    printf("\n=== Pyramide terminee ===\n");
+
+    return 0;
+}
 
