b623ba18da4bb2f20bcde0bf34dcdb978a0ae5a7
dioufndeyefatou623-web <dioufndeyefatou623@gmail.com>
2025-11-19 14:25:20 +0100

    Update bits.c

--- TP2/src/bits.c
+++ TP2/src/bits.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
-#include <limits.h>
 
 int main(void) {
 
     int d = 0x00F00000;
 
-    printf("=== Verification des bits ===\n");
-    printf("Valeur de d : %d\n", d);
+    int bit4  = (d >> (31 - 3)) & 1;
+    int bit20 = (d >> (31 - 19)) & 1;
 
-    int bitGauche4  = 31 - 3;
-    int bitGauche20 = 31 - 19;
+    int resultat = (bit4 == 1 && bit20 == 1) ? 1 : 0;
 
-    int valeurBit4  = (d >> bitGauche4) & 1;
-    int valeurBit20 = (d >> bitGauche20) & 1;
-
-    printf("Valeur du 4eme bit  (depuis la gauche)  : %d\n", valeurBit4);
-    printf("Valeur du 20eme bit (depuis la gauche) : %d\n", valeurBit20);
-
-    int resultat = (valeurBit4 == 1 && valeurBit20 == 1) ? 1 : 0;
-
-    printf("Resultat final : %d\n", resultat);
-    printf("=== Verification terminee ===\n");
+    printf("%d\n", resultat);
 
     return 0;
 }
+
